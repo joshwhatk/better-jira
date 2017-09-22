2a80d5a5442834413ba0b1b3c553e97ffcf57c62
Joshua Friend <josh.friend@daveramsey.com>
2017-09-22 07:54:02 -0500

    Make dynamic Standup Mode + bug fixes
    * Initial start
      * Make a way to focus on each card, one at a time
      * Perhaps make a dark background that blurs other cards so we can focus on the one specific card.
    * Working through improved standup mode
        * Namespace all CSS changes (enables actual toggling
        * resize and mark all columns including the header column
        * Set Shift + ←/→ to walk the board
        * Add instructions to the bottom right corner that appear on hover
    * Make Shippable
    * Prepare for production

--- src/js/better-jira.js
+++ src/js/better-jira.js
@@ -1,96 +1,103 @@
-(function()
+import Standup from './components/Standup';
+
+class BetterJira
 {
-  'use strict';
-
-  class BetterJira
-  {
-    constructor() {
-      this.data = {};
-      this.Storage = chrome.storage.sync;
-    }
-
-    initiate() {
-      window.removeEventListener('load', this.initiate);
-      console.log('Page loaded, running Better JIRA now.');
-
-      setTimeout(this._resizeColumns.bind(this), 700);
-    }
-
-    updateColumnWidths(detail) {
-      if(!detail.enabled) {
-        document.documentElement.style.setProperty('--viewport-width', 'inherit');
-        return;
-      }
-      this._getPreferredWidth(detail)
-    }
-
-    handleStandup(state) {
-      let cssClass = 'standup';
-      if(state) {
-        document.getElementsByTagName('body')[0].classList.add(cssClass);
-      } else {
-        document.getElementsByTagName('body')[0].classList.remove(cssClass);
-      }
-    }
-
-    _resizeColumns() {
-      if(document.querySelector('.ghx-swimlane') === null) {
-        return;
-      }
-
-      //-- Disallow setting columns if the plugin is not enabled
-      this.Storage.get('enabled', (storage) => {
-        if(!storage.enabled) {
-          document.documentElement.style.setProperty('--viewport-width', 'inherit');
-          return;
-        }
-
-        this.Storage.get('columnWidth', this._getPreferredWidth.bind(this));
-      });
-
-      this.Storage.get('standup', (storage) => {
-        this.handleStandup(storage.standup);
-      });
-    }
-
-    _getPreferredWidth(storage) {
-      this.data.columnWidth = storage.columnWidth;
-
-      if(isNaN(this.data.columnWidth)) {
-        this.data.columnWidth = 200;
-      }
-
-      this._setPreferredWidth();
-    }
-
-
-    _setPreferredWidth() {
-      let preferredWidth, columnCount, padding, width;
-
-      preferredWidth = this.data.columnWidth;
-
-      columnCount = document.querySelector('.ghx-swimlane > .ghx-columns')
-        .querySelectorAll('.ghx-column')
-        .length;
-
-      padding = columnCount * 12;
-      width = (columnCount * preferredWidth) + padding;
-
-      if(width < 10) {
-        return;
-      }
-
-      document.documentElement.style.setProperty('--viewport-width', `${width}px`);
-    }
+  constructor() {
+    this.data = {};
+    this.Storage = chrome.storage.sync;
   }
 
-  let app = new BetterJira();
-  window.addEventListener('load', app.initiate.bind(app));
+  preemptiveStrike() {
+    document.body.classList.add('better-jira');
+    this.Storage.get('poolWidth', (storage) => {
+      if(storage.poolWidth < 10 || isNaN(storage.poolWidth)) {
+        return;
+      }
+      console.log('Preemptively setting width now');
+      this._setPoolWidth(storage.poolWidth);
+    });
+  }
 
-  document.addEventListener('better-jira:updated', (event) => {
-    console.log('woohoo!', event.detail);
-    app.updateColumnWidths(event.detail);
+  initiate() {
+    window.removeEventListener('load', this.initiate, false);
+    console.log('Page loaded, running Better JIRA now.');
 
-    app.handleStandup(event.detail.standup);
-  });
-})();
+    setTimeout(this._resizeColumns.bind(this), 700);
+  }
+
+  updateColumnWidths(detail) {
+    if(!detail.enabled) {
+      document.body.classList.remove('better-jira');
+      return;
+    }
+    document.body.classList.add('better-jira');
+    this._getPreferredWidth(detail)
+  }
+
+  _resizeColumns() {
+    if(document.querySelector('.ghx-swimlane') === null) {
+      return;
+    }
+
+    //-- Disallow setting columns if the plugin is not enabled
+    this.Storage.get('enabled', (storage) => {
+      if(!storage.enabled) {
+        document.body.classList.remove('better-jira');
+        return;
+      }
+      document.body.classList.add('better-jira');
+
+      this.Storage.get('columnWidth', this._getPreferredWidth.bind(this));
+    });
+
+    this.Storage.get('standup', (storage) => {
+      Standup.run(storage.standup);
+    });
+  }
+
+  _getPreferredWidth(storage) {
+    this.data.columnWidth = storage.columnWidth;
+
+    if(isNaN(this.data.columnWidth)) {
+      this.data.columnWidth = 200;
+    }
+
+    this._setPreferredWidth();
+  }
+
+
+  _setPreferredWidth() {
+    let preferredWidth, columnCount, padding, width;
+
+    preferredWidth = this.data.columnWidth;
+
+    columnCount = document.querySelector('.ghx-swimlane > .ghx-columns')
+      .querySelectorAll('.ghx-column')
+      .length;
+
+    padding = columnCount * 12;
+    width = (columnCount * preferredWidth) + padding;
+
+    if(width < 10) {
+      return;
+    }
+    this.Storage.set({poolWidth: width});
+
+    this._setPoolWidth(width);
+  }
+
+  _setPoolWidth(width) {
+    document.documentElement.style.setProperty('--viewport-width', `${width}px`);
+  }
+}
+
+let app = new BetterJira();
+window.addEventListener('load', app.initiate.bind(app), false);
+app.preemptiveStrike();
+
+document.addEventListener('better-jira:updated', (event) => {
+  console.log('woohoo!', event.detail);
+  app.updateColumnWidths(event.detail);
+
+  Standup.run(event.detail.standup);
+});
