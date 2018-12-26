0ebce5a0f531f030dacac68d063618681abde25a
joshwhatk <joshwhatk@gmail.com>
2018-12-25 21:29:38 -0600

    Feature: Open new tab for all Github Links
    * Also, fix “Create Pull Request” link to actually compare the branch

--- src/js/components/OpenGithubInNewTab.js
+++ src/js/components/OpenGithubInNewTab.js
@@ -5,13 +5,30 @@ export default class OpenGithubInNewTab {
 
   constructor() {
     document.addEventListener('click', (click) => {
-      let githubLink = click.target.closest('.pullrequest-link');
+      let githubLink = click.target.closest(
+        '.pullrequest-link, .repository-link, .branch-link, .create-pullrequest-link, .changesetid, .filename a, .filerow a.more-files-info'
+      );
       if (!githubLink) {
         return;
+      }
+
+      if (githubLink.classList.contains('create-pullrequest-link')) {
+        this.enableBetterCreatePullRequestLink(githubLink);
       }
 
       click.preventDefault();
       window.open(githubLink.href, '_blank');
     });
+  }
+
+  enableBetterCreatePullRequestLink(githubLink) {
+    try {
+      let branchName = githubLink.parentElement.parentElement.querySelector(
+        '.branch-link'
+      ).title;
+      githubLink.href = `${githubLink.href}/master...${encodeURI(branchName)}`;
+    } catch (e) {
+      console.warn(e);
+    }
   }
 }
