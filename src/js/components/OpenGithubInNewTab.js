5e6de28e8e3296367f7f3e08acde7fdb23b67da8
Josh Friend <josh.friend@daveramsey.com>
2019-01-03 12:38:52 -0600

    Feature: Open new tab for all Github Links (#9)
    * Feature: Open new tab for all Github Links
    * Fix “Create Pull Request” link to actually compare the branch to master
    * preventDefault before creating PR Link
    * Add noopener to the githubLinks
    * Bump to version 1.4.1

--- src/js/components/OpenGithubInNewTab.js
+++ src/js/components/OpenGithubInNewTab.js
@@ -5,13 +5,31 @@ export default class OpenGithubInNewTab {
 
   constructor() {
     document.addEventListener('click', (click) => {
-      let githubLink = click.target.closest('.pullrequest-link');
+      let githubLink = click.target.closest(
+        '.pullrequest-link, .repository-link, .branch-link, .create-pullrequest-link, .changesetid, .filename a, .filerow a.more-files-info'
+      );
       if (!githubLink) {
         return;
       }
 
       click.preventDefault();
-      window.open(githubLink.href, '_blank');
+      if (githubLink.classList.contains('create-pullrequest-link')) {
+        this.enableBetterCreatePullRequestLink(githubLink);
+      }
+
+      let newWindow = window.open(githubLink.href, '_blank');
+      newWindow.opener = null;
     });
+  }
+
+  enableBetterCreatePullRequestLink(githubLink) {
+    try {
+      let branchName = githubLink.parentElement.parentElement.querySelector(
+        '.branch-link'
+      ).title;
+      githubLink.href = `${githubLink.href}/master...${encodeURI(branchName)}`;
+    } catch (e) {
+      console.warn(e);
+    }
   }
 }
