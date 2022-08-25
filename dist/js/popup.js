b4652e99d10ba215375bd3a689770d57ea66f7b3
Josh Friend <josh.friend@ramseysolutions.com>
2022-08-25 15:47:00 -0500

    Fix OpenGithubInNewTab and style fixes for content and standup

--- dist/js/popup.js
+++ dist/js/popup.js
@@ -1,1 +1,216 @@
-!function(e){var t={};function n(a){if(t[a])return t[a].exports;var o=t[a]={i:a,l:!1,exports:{}};return e[a].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,a){n.o(e,t)||Object.defineProperty(e,t,{configurable:!1,enumerable:!0,get:a})},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="/",n(n.s=5)}({5:function(e,t,n){e.exports=n(6)},6:function(e,t){var n=function(){function e(e,t){for(var n=0;n<t.length;n++){var a=t[n];a.enumerable=a.enumerable||!1,a.configurable=!0,"value"in a&&(a.writable=!0),Object.defineProperty(e,a.key,a)}}return function(t,n,a){return n&&e(t.prototype,n),a&&e(t,a),t}}();new(function(){function e(){!function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}(this,e),this.Storage=chrome.storage.sync,this.data={},this.defaults={columnWidth:200,enabled:!0,standup:!1,updatedEvent:"better-jira:updated"},this.loadListener=this.boot.bind(this),window.addEventListener("load",this.loadListener)}return n(e,[{key:"boot",value:function(){window.removeEventListener("load",this.loadListener),this.setDefaults(),this.handleFormEvents()}},{key:"setDefaults",value:function(){var e=this;this.Storage.get("columnWidth",function(t){console.log("storage",t);var n=t.columnWidth;console.log("columnWidth from Storage",n),n||(n=e.defaults.columnWidth,e.Storage.set({columnWidth:n})),e.data.columnWidth=n,document.getElementById("columnWidth").value=e.data.columnWidth}),this.Storage.get("enabled",function(t){var n=t.enabled;void 0===n&&(n=e.defaults.enabled,e.Storage.set({enabled:n})),e.data.enabled=n,console.log("enabled storage",t),document.getElementById("enabled").checked=!!n}),this.Storage.get("standup",function(t){var n=t.standup;void 0===n&&(n=e.defaults.standup,e.Storage.set({standup:n})),e.data.standup=n,console.log("standup storage",t),document.getElementById("standup").checked=!!n})}},{key:"handleFormEvents",value:function(){var e=this,t=arguments;document.getElementById("close").addEventListener("click",function(e){window.close()});var n=document.getElementById("enabled");n.addEventListener("click",function(t){e.data.enabled=n.checked,e.save()});var a=document.getElementById("standup");a.addEventListener("click",function(t){console.log("you clicked it!",a.checked,t),e.data.standup=a.checked,e.save()}),document.getElementById("better-jira").addEventListener("submit",function(n){n.preventDefault(),console.log("form submission information",t),e.data.columnWidth=document.getElementById("columnWidth").value,e.data.enabled=document.getElementById("enabled").checked,e.save()})}},{key:"save",value:function(){var e=this;this.Storage.set({enabled:this.data.enabled},function(){e.Storage.set({columnWidth:e.data.columnWidth},function(){e.Storage.set({standup:e.data.standup},e.refresh.bind(e))})})}},{key:"refresh",value:function(){var e=["(function()","{","'use strict';","let updateEvent = new CustomEvent('"+this.defaults.updatedEvent+"', {","detail: {","columnWidth: "+this.data.columnWidth+",","enabled: "+this.data.enabled+",","standup: "+this.data.standup+",","}","});","document.dispatchEvent(updateEvent);","})();"];chrome.tabs.executeScript({code:e.join("")})}}]),e}())}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 10);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 10:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(11);
+
+
+/***/ }),
+
+/***/ 11:
+/***/ (function(module, exports) {
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Popup = function () {
+  function Popup() {
+    _classCallCheck(this, Popup);
+
+    this.Storage = chrome.storage.sync;
+    this.data = {};
+    this.defaults = {
+      columnWidth: 200,
+      enabled: true,
+      standup: false,
+      updatedEvent: 'better-jira:updated'
+    };
+
+    this.loadListener = this.boot.bind(this);
+    window.addEventListener('load', this.loadListener);
+  }
+
+  _createClass(Popup, [{
+    key: 'boot',
+    value: function boot() {
+      window.removeEventListener('load', this.loadListener);
+
+      this.setDefaults();
+      this.handleFormEvents();
+    }
+  }, {
+    key: 'setDefaults',
+    value: function setDefaults() {
+      var _this = this;
+
+      this.Storage.get('columnWidth', function (storage) {
+        console.log('storage', storage);
+        var value = storage.columnWidth;
+        console.log('columnWidth from Storage', value);
+        if (!value) {
+          value = _this.defaults.columnWidth;
+          _this.Storage.set({ columnWidth: value });
+        }
+        _this.data.columnWidth = value;
+
+        document.getElementById('columnWidth').value = _this.data.columnWidth;
+      });
+
+      this.Storage.get('enabled', function (storage) {
+        var value = storage.enabled;
+        if (value === undefined) {
+          value = _this.defaults.enabled;
+          _this.Storage.set({ enabled: value });
+        }
+
+        _this.data.enabled = value;
+        console.log('enabled storage', storage);
+
+        document.getElementById('enabled').checked = !!value;
+      });
+
+      this.Storage.get('standup', function (storage) {
+        var value = storage.standup;
+        if (value === undefined) {
+          value = _this.defaults.standup;
+          _this.Storage.set({ standup: value });
+        }
+
+        _this.data.standup = value;
+        console.log('standup storage', storage);
+
+        document.getElementById('standup').checked = !!value;
+      });
+    }
+  }, {
+    key: 'handleFormEvents',
+    value: function handleFormEvents() {
+      var _this2 = this,
+          _arguments = arguments;
+
+      //-- Close the window
+      var close = document.getElementById('close');
+      close.addEventListener('click', function (event) {
+        window.close();
+      });
+
+      //-- Trigger Enabled (as a 1-click event)
+      var enabled = document.getElementById('enabled');
+      enabled.addEventListener('click', function (event) {
+        _this2.data.enabled = enabled.checked;
+        _this2.save();
+      });
+
+      //-- Trigger Standup (as a 1-click event)
+      var standup = document.getElementById('standup');
+      standup.addEventListener('click', function (event) {
+        console.log('you clicked it!', standup.checked, event);
+        _this2.data.standup = standup.checked;
+        _this2.save();
+      });
+
+      document.getElementById('better-jira').addEventListener('submit', function (event) {
+        event.preventDefault();
+        console.log('form submission information', _arguments);
+        _this2.data.columnWidth = document.getElementById('columnWidth').value;
+        _this2.data.enabled = document.getElementById('enabled').checked;
+        _this2.save();
+      });
+    }
+  }, {
+    key: 'save',
+    value: function save() {
+      var _this3 = this;
+
+      this.Storage.set({ enabled: this.data.enabled }, function () {
+        _this3.Storage.set({ columnWidth: _this3.data.columnWidth }, function () {
+          _this3.Storage.set({ standup: _this3.data.standup }, _this3.refresh.bind(_this3));
+        });
+      });
+    }
+  }, {
+    key: 'refresh',
+    value: function refresh() {
+      var code = ['(function()', '{', '\'use strict\';', 'let updateEvent = new CustomEvent(\'' + this.defaults.updatedEvent + '\', {', 'detail: {', 'columnWidth: ' + this.data.columnWidth + ',', 'enabled: ' + this.data.enabled + ',', 'standup: ' + this.data.standup + ',', '}', '});', 'document.dispatchEvent(updateEvent);', '})();'];
+
+      chrome.tabs.executeScript({
+        code: code.join('')
+      });
+    }
+  }]);
+
+  return Popup;
+}();
+
+new Popup();
+
+/***/ })
+
+/******/ });
